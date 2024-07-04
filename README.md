63e61078b569223ea2c76fbd2a31ff0128010c4f
GravityTwoG <crytekov@gmail.com>
2024-07-04 23:11:08 +0700

    feat: setup manual acknowledgements

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Text To Speech As A Service
+
+## RabbitMQ
+
+- list messages statistics
+
+```sh
+rabbitmqctl list_queues name messages_ready messages_unacknowledged
+```
--- diffuser/diffuser.py
+++ diffuser/diffuser.py
@@ -11,11 +11,11 @@ RABBITMQ_PORT = 5672
 RABBITMQ_USERNAME = 'admin'
 RABBITMQ_PASSWORD = '12121212'
 
-GENERATION_REQUEST_QUEUE = 'generate_image'
-IMAGE_GENERATED_QUEUE = 'image_generated'
+GENERATION_REQUEST_QUEUE = 'image_generation_requests'
+IMAGE_GENERATED_QUEUE = 'image_generation_responses'
 
 def send_image_generated(
-  channel: pika.adapters.blocking_connection.BlockingChannel, 
+  ch: pika.adapters.blocking_connection.BlockingChannel, 
   generation_id: str, 
   num_images: int,
 ):
@@ -25,12 +25,19 @@ def send_image_generated(
     'generation_id': generation_id,
     'image_count': num_images
   }
-  channel.basic_publish(exchange='',
-                        routing_key=IMAGE_GENERATED_QUEUE,
-                        body=json.dumps(image_generated))
+  ch.basic_publish(
+    exchange='',
+    routing_key=IMAGE_GENERATED_QUEUE,
+    body=json.dumps(image_generated),
+  )
 
 
-def handle_generation_request(ch, method, properties, body):
+def handle_generation_request(
+  ch: pika.adapters.blocking_connection.BlockingChannel, 
+  method: pika.spec.Basic.Deliver, 
+  properties: pika.spec.BasicProperties, 
+  body: str,
+):
   print(f" [x] Received {body}")
   generate_request = json.loads(body)
   
@@ -39,6 +46,7 @@ def handle_generation_request(ch, method, properties, body):
     generate_request['generation_id'],
     generate_request['num_images']
   )
+  ch.basic_ack(delivery_tag = method.delivery_tag)
   print(f" [x] Image generated: {generate_request['generation_id']}")
 
   send_image_generated(
@@ -49,16 +57,23 @@ def handle_generation_request(ch, method, properties, body):
   print(" [x] Sent 'image_generated'")
   
 
-def send_generation_request(channel):
+def send_generation_request(
+  ch: pika.adapters.blocking_connection.BlockingChannel,
+):
   # send a message to queue "generate_image"
   generate_image_request = {
     "prompt": "A black and white image",
     "generation_id": "1234",
     "num_images": 1
   }
-  channel.basic_publish(exchange='',
-                        routing_key=GENERATION_REQUEST_QUEUE,
-                        body=json.dumps(generate_image_request))
+  ch.basic_publish(
+    exchange='',
+    routing_key=GENERATION_REQUEST_QUEUE,
+    body=json.dumps(generate_image_request),
+    properties=pika.BasicProperties(
+      delivery_mode=pika.DeliveryMode.Persistent
+    )
+  )
 
 
 def main():
@@ -72,23 +87,28 @@ def main():
 
   channel = connection.channel()
   print("Connected to RabbitMQ")
+  # don't dispatch a new message to a worker until it has processed and acknowledged the previous one
+  channel.basic_qos(prefetch_count=1)
 
   # create a queue (idempotent operation)
-  channel.queue_declare(queue=GENERATION_REQUEST_QUEUE)
-  channel.queue_declare(queue=IMAGE_GENERATED_QUEUE)
+  channel.queue_declare(queue=GENERATION_REQUEST_QUEUE, durable=True)
+  channel.queue_declare(queue=IMAGE_GENERATED_QUEUE, durable=True)
 
   send_generation_request(channel)
 
   # receive messages
-  channel.basic_consume(queue=GENERATION_REQUEST_QUEUE,
-                        auto_ack=True,
-                        on_message_callback=handle_generation_request)
+  channel.basic_consume(
+    queue=GENERATION_REQUEST_QUEUE,
+    auto_ack=False,
+    on_message_callback=handle_generation_request,
+  )
 
   print(' [*] Waiting for messages. To exit press CTRL+C')
   channel.start_consuming()
 
   # flush all buffers and close connection
   connection.close()
+
 
 if __name__ == '__main__':
   try:
